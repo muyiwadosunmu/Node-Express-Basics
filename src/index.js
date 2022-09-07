487e6a4e6336a8176b20f5af96488fca23779f55
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-07 11:44:33 +0100

    Handling post requests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 const express = require("express");
 
 const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(PORT, () => console.log(`Running express server on port ${PORT}`));
+
+const groceryList = [
+  {
+    item: "milk",
+    quantity: 2,
+  },
+  {
+    item: "cereal",
+    quantity: 5,
+  },
+  {
+    item: "pop-tarts",
+    quantity: 6,
+  },
+];
+
+app.get("/", (req,res) => {
+  res.send(groceryList);
+});
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  groceryList.push(req.body)
+  res.send(201);
+});
