3c96c9b1b590b241b0788c3a52a58de2ca362bd7
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-08 13:29:21 +0100

    Dealing with sessions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const groceriesRoute = require("./routes/groceries");
 const marketsRoute = require("./routes/markets");
 
@@ -6,21 +8,26 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "qwertyuiopassdfghjklzxcvbnm",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use((req,res, next) => {
-    console.log(`${req.method}:${req.url}`);
-    next()
+app.use((req, res, next) => {
+  console.log(`${req.method}:${req.url}`);
+  next();
 });
 
 app.use("/api/v1/groceries", groceriesRoute);
 app.use("/api/v1/markets", marketsRoute);
 
 app.listen(PORT, () => console.log(`Running express server on port ${PORT}`));
-
-
-
-
 
 
--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -17,19 +17,45 @@ const groceryList = [
   },
 ];
 
-router.get("", (req, res) => {
+router.get("/", (req, res) => {
+  res.cookie("visuted", true, {
+    maxAge: 10000,
+  })
   res.send(groceryList);
 });
 
 router.get("/:item", (req, res) => {
+  console.log(req.cookies);
   const { item } = req.params;
   const groceryItem = groceryList.find((g) => g.item === item);
   res.send(groceryItem);
 });
 
-router.post("", (req, res) => {
+router.post("/", (req, res) => {
   console.log(req.body);
   groceryList.push(req.body);
+  res.send(201);
+});
+
+router.get("/shopping/cart", (req,res) => {
+  const { cart } = req.session;
+  if (!cart) {
+    res.send("You have no cart session")
+  } else {
+    res.send(cart);
+  }
+});
+router.post("/shopping/cart/item", (req,res) => {
+  const { item, quantity } = req.body;
+  const cartItem = { item, quantity };
+  const { cart } = req.session;
+  if ( cart ) {
+    req.session.cart.items.push(cartItem);
+  } else {
+    req.session.cart = {
+      items : [cartItem],
+    }
+  }
   res.send(201);
 });
 
