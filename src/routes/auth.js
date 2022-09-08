8294aab68f95a63633433fc03713fa5efc6d08cf
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-08 16:52:42 +0100

    mongoDB, mongoose

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,20 +4,27 @@ const router = Router();
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  if (username && password ) {
+  if (username && password) {
     if (req.session.user) {
-        res.send(req.session.user);
+      res.send(req.session.user);
     } else {
-        req.session.user = {
-            username,
-        }; res.send(req.session)
+      req.session.user = {
+        username,
+      };
+      res.send(req.session);
     }
-  } else res.send(401)
+  } else res.send(401);
 });
 
-router.post("/register", (req,res) => {
-    const { username, password, email } = req.body;  
-
-})
+router.post("/register", async (req, res) => {
+  const { username, password, email } = req.body;
+  const userDB = await User.findOne({ $or: [{ username }, { email }] });
+  if (userDB) {
+    res.status(400).send({ msg: "User already exists" });
+  } else {
+    const newUser = await User.create({ username, password, email });
+    newUser.save();
+  }
+});
 
 module.exports = router;
