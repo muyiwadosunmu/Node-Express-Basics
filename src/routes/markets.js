2c4da960be2121e078652d0a689206af18af16fc
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-08 10:43:25 +0100

    query params

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -3,18 +3,40 @@ const { Router } = require("express");
 const router = Router()
 const superMarkets = [
     {
+        id: 1,
         store: "Whole Foods",
+        miles: 0.6,
     },
     {
+        id: 2,
         store: "Trader Joes",
+        miles: 2.5,
     },
     {
+        id: 3,
         store: "Albertsons",
+        miles:2.8
+    },
+    {
+        id: 4,
+        store: "Trader Jones",
+        miles:3.5
+    },
+    {
+        id: 3,
+        store: "Albertsons",
+        miles:1.8
     }
 ];
 
+
 router.get('/', (req,res) => {
-    res.send(superMarkets);
+    const {miles} = req.query;
+    const parsedMiles = parseInt(miles);
+    if (!isNaN(parsedMiles)) {
+        const filteredStores = superMarkets.filter((s) => s.miles <= parsedMiles);
+        res.send(filteredStores);
+    } else res.send(superMarkets);
 })
 
 
