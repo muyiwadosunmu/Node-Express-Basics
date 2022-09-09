ba036a989ff02124ebb9a4b7e86ba7aa25c35b29
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-09 10:59:37 +0100

    basic hashing

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,8 @@
 const { Router, response } = require("express");
 const User = require("../database/schemas/User");
 const router = Router();
+const { hashPassword } = require("../utils/helpers"); 
+
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
@@ -22,6 +24,8 @@ router.post("/register", async (req, res) => {
   if (userDB) {
     res.status(400).send({ msg: "User already exists" });
   } else {
+    const password = hashPassword(req.body.password);
+    console.log(password);
     const newUser = await User.create({ username, password, email });
     res.sendStatus(201);
   }
